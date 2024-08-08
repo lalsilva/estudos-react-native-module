5b7bdd1cb31720cb916045a55b7aecdc01a77732
Luiz Silva <lalsilva@gmail.com>
2024-08-08 11:35:35 -0300

    Adicionados os métodos getter e setter do tema do dispositivo.

--- example/App.tsx
+++ example/App.tsx
@@ -1,10 +1,18 @@
 import * as Settings from "expo-settings";
-import { Text, View } from "react-native";
+import { Button, Text, View } from "react-native";
 
 export default function App() {
+  const theme = Settings.getTheme();
+  // Toggle between dark and light theme
+  const nextTheme = theme === "dark" ? "light" : "dark";
+
   return (
     <View style={{ flex: 1, alignItems: "center", justifyContent: "center" }}>
       <Text>Theme: {Settings.getTheme()}</Text>
+      <Button
+        title={`Set theme to ${nextTheme}`}
+        onPress={() => Settings.setTheme(nextTheme)}
+      />
     </View>
   );
 }
--- src/index.ts
+++ src/index.ts
@@ -3,3 +3,7 @@ import ExpoSettingsModule from "./ExpoSettingsModule";
 export function getTheme(): string {
   return ExpoSettingsModule.getTheme();
 }
+
+export function setTheme(theme: string): void {
+  return ExpoSettingsModule.setTheme(theme);
+}
